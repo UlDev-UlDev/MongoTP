174f839a1302970474e65a171c70f0cfc36e22e5
Vaala <brice.bitot@neuf.fr>
2020-11-25 09:19:06 +0100

    fix

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.post('/import/file',(req, res) => {
 
 app.get('/import/:id', (req, res) => {
     let id = req.params.id;
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/table.html');
 });
 
 app.get('/api/:id', (req, res) => {
