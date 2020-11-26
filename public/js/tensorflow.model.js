13aa6e6ad5040563b2cfd3e4bff59f87b0f34048
Beaurval <beaury.valentin@gmail.com>
2020-11-26 15:40:08 +0100

    spinner + auto predict

---
+++ public/js/tensorflow.model.js
@@ -0,0 +1,30 @@
+// More API functions here:
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+
+// the link to your model provided by Teachable Machine export panel
+const URL = '/';
+
+let model, labelContainer, maxPredictions;
+
+// Load the image model and setup the webcam
+async function init() {
+    const modelURL = URL + 'model.json';
+    const metadataURL = URL + 'metadata.json';
+
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // or files from your local hard drive
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+}
+
+// run the webcam image through the image model
+async function predict() {
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(document.getElementById("myimage"));
+    console.log(prediction)
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction =
+            prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
+    }
+}
